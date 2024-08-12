246e74480fa5cdb48461247bf51485f7f50cb853
LDA <ld+github@spyrit.net>
2024-08-12 15:46:26 +0200

    wip

--- DependencyInjection/SpyritPropelDatagridExtension.php
+++ DependencyInjection/SpyritPropelDatagridExtension.php
@@ -14,10 +14,7 @@ use Symfony\Component\DependencyInjection\Loader;
  */
 class SpyritPropelDatagridExtension extends Extension
 {
-    /**
-     * {@inheritDoc}
-     */
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container): void
     {
         $configuration = new Configuration();
         $this->processConfiguration($configuration, $configs);
